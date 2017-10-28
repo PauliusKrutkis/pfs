12fdcb8dcf9548c1a3fe8bcd0792909d68f3f8f4
Paulius Krutkis <paulius.krutkis@gmail.com>
2017-10-28 23:24:37 +0300

    plugin description

--- pfs.php
+++ pfs.php
@@ -1,7 +1,12 @@
 <?php
-/*
-Plugin Name: PFS
-*/
+/**
+ * Plugin Name: PFS
+ * Description: Simple wordpress post filter system
+ * Version: 1.0.0
+ * Author: Paulius Krutkis
+ * Author URI: https://github.com/PauliusKrutkis
+ * License: GPLv2
+ */
 
 foreach (glob(plugin_dir_path(__FILE__) . "src/*.php") as $file) {
     include_once $file;
